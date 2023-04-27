f8b4bc90775ad11be5e50f37ee36e24c52ca2541
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-28 03:24:50 +0530

    adding resize fucn

--- markattendaance.py
+++ markattendaance.py
@@ -3,6 +3,13 @@ import numpy as np
 import face_recognition as face_rec
 import os
 
+
+def resize(img, size) :
+    width = int(img.shape[1]*size)
+    height = int(img.shape[0] * size)
+    dimension = (width, height)
+    return cv2.resize(img, dimension, interpolation= cv2.INTER_AREA)
+
 path = 'images'
 studentimg = []
 studentname = []
