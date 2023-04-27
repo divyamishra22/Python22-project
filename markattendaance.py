d15e300361cb88a0d2b8f723215f9ef7647eb7e4
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-28 03:17:10 +0530

    taking 1 img at a time and adding to list

--- markattendaance.py
+++ markattendaance.py
@@ -4,7 +4,10 @@ import face_recognition as face_rec
 import os
 
 path = 'images'
-studentImg = []
+studentimg = []
 studentName = []
 myList = os.listdir(path)
 print(myList)
+for cl in myList :
+    img = cv2.imread(f'{path}/{cl}')
+    studentimg.append(img)
