316c60d8e2224b986bebc0a77df1e9375a2b68bd
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-28 03:19:09 +0530

    remove.jpg from img name

--- markattendaance.py
+++ markattendaance.py
@@ -5,9 +5,10 @@ import os
 
 path = 'images'
 studentimg = []
-studentName = []
+studentname = []
 myList = os.listdir(path)
 print(myList)
 for cl in myList :
     img = cv2.imread(f'{path}/{cl}')
     studentimg.append(img)
+    studentname.append(os.path.splitext(cl)[0])
